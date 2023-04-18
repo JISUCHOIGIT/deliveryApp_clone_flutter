1920b3e362cb3c86de7d33c12d84a022a28587fe
JISUOGIT <wltn203@naver.com>
2023-04-18 22:01:00 +0900

    20230418 - TabBarView - TabBarView index 변경 > tabListener 통해 setState 구현

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -9,15 +9,71 @@ class RootTab extends StatefulWidget {
   State<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends State<RootTab> {
+class _RootTabState extends State<RootTab> with TickerProviderStateMixin {
+  // late : 나중에 값을 부를때 controller 선언이 되어 있을 거라는 명시
+  late TabController controller;
   int index = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // vsync : 현재 state 해당하는 걸 넣어줘야 함
+    // with TickerProviderStateMixin 넣어줘야 함
+    // 애니메이션 관련 컨트롤러는 이렇게 설정하는 음우가 많음
+    // controller는 가장먼저 선언해야함
+    controller = TabController(length: 4, vsync: this);
+
+    // tabBarView index이동 하기 위한 addListener 함수
+    // tabListener 통해서
+    controller.addListener(tabListener);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.removeListener(tabListener);
+  }
+
+  // tabListener 안에서 index setState해주기
+  // 현재 index -> controller.index 넣어주기
+  void tabListener(){
+    setState(() {
+      index = controller.index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '코팩딜리버리',
-      child: Center(
-        child: Text('Root Tab'),
+      // TabBar 해당 View
+      child: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: [
+          Center(
+            child: Container(
+              child: Text('홈'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('음식'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('주문'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('프로필'),
+            ),
+          ),
+        ],
       ),
       // bottomNavigationBar 속성
       bottomNavigationBar: BottomNavigationBar(
@@ -34,9 +90,7 @@ class _RootTabState extends State<RootTab> {
         // 탭을 클릭한 index 숫자
         onTap: (int index){
           // 클릭한 index 현재 index에 대입하여 바꿔주기 + setState
-          setState(() {
-            this.index = index;
-          });
+            controller.animateTo(index);
         },
         // 현재 index 정보
         currentIndex: index,
